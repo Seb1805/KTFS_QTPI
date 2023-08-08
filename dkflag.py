e9568a0c93ab372e54fda75c16879cc3df617aa5
Frank <frankjensen156@gmail.com>
2023-08-08 11:31:21 +0200

    fill function test

--- dkflag.py
+++ dkflag.py
@@ -25,6 +25,7 @@ def drawMainBoard():
 #Draw the main board
 drawMainBoard()
 
+fillSpace(1,1,(128,0,0))
 
 #Corner 1 Example
 # sense.set_pixel(0,1,(255,0,0))
@@ -33,5 +34,16 @@ drawMainBoard()
 # sense.set_pixel(0,0,(255,0,0))
 
 
+
+def fillSpace(x, y color):
+
+    row = (x-1)*3
+    col = (y-1)*3
+
+
+    sense.set_pixel(row, col, color) 
+    sense.set_pixel(row + 1, col, color) 
+    sense.set_pixel(row, col + 1, color) 
+    sense.set_pixel(row + 1, col + 1, color) 
 
 
