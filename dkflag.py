a01736d48d57cbeb52a244be44d902cffb45a951
PiPc <a@a.dk>
2023-08-08 10:56:33 +0200

    Added drawmain board

--- dkflag.py
+++ dkflag.py
@@ -1,1 +1,37 @@
-#Hello
+from sense_hat import SenseHat
+import time
+from PIL import Image
+import os
+
+sense = SenseHat()
+
+
+def drawMainBoard():
+    # Open image file
+    image_file = os.path.join(
+    os.sep,"/home","ktfs/SenseHAT","4.png")                                                                                                                                                                                                                        
+    img = Image.open(image_file)
+
+    sense.load_image(image_file)
+    rgb_img = img.convert('RGB')
+    # Generate rgb values for image pixels
+    image_pixels = list(rgb_img.getdata())
+
+
+
+
+
+
+#Draw the main board
+drawMainBoard()
+
+
+#Corner 1 Example
+# sense.set_pixel(0,1,(255,0,0))
+# sense.set_pixel(1,0,(255,0,0))
+# sense.set_pixel(1,1,(255,0,0))
+# sense.set_pixel(0,0,(255,0,0))
+
+
+
+
